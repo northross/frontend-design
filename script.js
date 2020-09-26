84944f02cfb8e3d604074737a50ff3464c702de5
northross <rossnorthrup@yahoo.com>
2020-09-26 15:41:41 -0400

    added jquery handlers and functions to show hide and decorate buttons upon select

--- script.js
+++ script.js
@@ -1,28 +1,56 @@
-console.log('hello');
+'use strict';
 
 const onShowAll = (e) => {
   e.preventDefault();
-  console.log('hello');
+  console.log('here');
+  // ALL
+  $('.show-all').removeClass('btn');
+  $('.show-all').addClass('select');
+  // Even
+  $('.show-even').removeClass('select');
+  $('.show-even').addClass('btn');
   $('.even').show();
+  // ODD
+  $('.show-odd').removeClass('select');
+  $('.show-odd').addClass('btn');
   $('.odd').show();
 };
 
 const onShowEven = (e) => {
   e.preventDefault();
-  console.log('evev');
+  // EVEN
   $('.even').show();
+  $('.show-even').removeClass('btn');
+  $('.show-even').addClass('select');
+  // ODD
   $('.odd').hide();
+  $('.show-odd').removeClass('select');
+  $('#odd').addClass('btn');
+  // ALL
+  $('.show-all').removeClass('select');
+  $('.show-all').addClass('btn');
 };
 
 const onShowOdd = (e) => {
   e.preventDefault();
-  console.log('odd');
-  $('.even').hide();
+  // ODD
   $('.odd').show();
+  $('.show-odd').removeClass('btn');
+  $('#odd').addClass('select');
+  // EVEN
+  $('.even').hide();
+  $('.show-even').removeClass('select');
+  $('.show-even').addClass('btn');
+  // ALL
+  $('.show-all').removeClass('select');
+  $('.show-all').addClass('btn');
 };
 
-const addHandlers = () => {
+// Event Handlers
+$(() => {
+  $('.show-even').show();
+  $('.show-odd').show();
   $('.show-all').on('click', onShowAll);
   $('.show-even').on('click', onShowEven);
   $('.show-odd').on('click', onShowOdd);
-};
+});
