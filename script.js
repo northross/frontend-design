e293edb459cb93ec160dd09e88b50b1ebe02baf6
northross <rossnorthrup@yahoo.com>
2020-09-27 21:46:09 -0400

    Updates

---
+++ script.js
@@ -0,0 +1,73 @@
+'use strict';
+
+// Show All Boxes
+const onShowAll = (e) => {
+  e.preventDefault();
+  console.log('here');
+  // ALL
+  $('.show-all').removeClass('btn');
+  $('.show-all').addClass('select');
+  // Even
+  $('.show-even').removeClass('select');
+  $('.show-even').addClass('btn');
+  $('.even').show();
+  // ODD
+  $('.show-odd').removeClass('select');
+  $('.show-odd').addClass('btn');
+  $('.odd').show();
+};
+
+// Show Even Boxes
+const onShowEven = (e) => {
+  e.preventDefault();
+  // EVEN
+  $('.even').show();
+  $('.show-even').removeClass('btn');
+  $('.show-even').addClass('select');
+  // ODD
+  $('.odd').hide();
+  $('.show-odd').removeClass('select');
+  $('.show-odd').addClass('btn');
+  // ALL
+  $('.show-all').removeClass('select');
+  $('.show-all').addClass('btn');
+};
+
+// Show Odd Boxes
+const onShowOdd = (e) => {
+  e.preventDefault();
+  // ODD
+  $('.odd').show();
+  $('.show-odd').removeClass('btn');
+  $('.show-odd').addClass('select');
+  // EVEN
+  $('.even').hide();
+  $('.show-even').removeClass('select');
+  $('.show-even').addClass('btn');
+  // ALL
+  $('.show-all').removeClass('select');
+  $('.show-all').addClass('btn');
+};
+
+// Event Handlers
+$(() => {
+  // Show Click Events
+  $('.show-all').on('click', onShowAll);
+  $('.show-even').on('click', onShowEven);
+  $('.show-odd').on('click', onShowOdd);
+  // Show Odd and Even Divs
+  $('.show-even').show();
+  $('.show-odd').show();
+  // Show All btn on Select
+  $('.show-all').removeClass('btn');
+  $('.show-all').addClass('select');
+  // Navbar open menu toggle intended for opening the nav menu
+  //   $('.nav-menu').toggle(
+  //     function () {
+  //       $('.navbar-links').addClass('active');
+  //     },
+  //     function () {
+  //       $('.navbar-links').removeClass('active');
+  //     }
+  //   );
+});
